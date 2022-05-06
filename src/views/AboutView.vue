f798ba7473357216a95687eed5101fed97952af4
Carrie <echocarriet@gmail.com>
2022-05-06 17:38:28 +0800

    產生label問題

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,27 @@
 <template>
-  <div class="about">
+  <div class="about container">
     <h1>This is an about page</h1>
+    <!-- BS Form controls -->
+    <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label">Email address</label>
+      <input
+        type="email"
+        class="form-control"
+        id="exampleFormControlInput1"
+        placeholder="name@example.com"
+      />
+    </div>
+    <!-- input 標籤放置 label 中 -->
+    <!-- <div class="mb-3">
+      <label for="exampleFormControlInput1" class="form-label"
+        >Email address
+        <input
+          type="email"
+          class="form-control"
+          id="exampleFormControlInput1"
+          placeholder="name@example.com"
+        />
+      </label>
+    </div> -->
   </div>
 </template>
